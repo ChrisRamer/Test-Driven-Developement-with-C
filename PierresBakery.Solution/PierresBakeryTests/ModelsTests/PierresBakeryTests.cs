d34b28532135926968f58b2ee18a0acdf6e3470c
Chris Ramer <chrisramer.97@gmail.com>
2021-01-22 09:50:38 -0800

    Make tests (before code)

--- PierresBakery.Solution/PierresBakeryTests/ModelsTests/PierresBakeryTests.cs
+++ PierresBakery.Solution/PierresBakeryTests/ModelsTests/PierresBakeryTests.cs
@@ -8,6 +8,30 @@ namespace PierresBakeryTests
 	[TestClass]
 	public class BakeryTests
 	{
-		//
+		[TestMethod]
+		public void GetCurrentOrder_GetOrderItems_List()
+		{
+			Bread bread = new Bread();
+			bread.MakeOrder(3);
+			Pastry pastry = new Pastry();
+			pastry.MakeOrder(2);
+
+			string result = bread.GetCurrentOrder().items;
+
+			Assert.AreEqual(result, "bread x3, pastry x2");
+		}
+
+		[TestMethod]
+		public void GetCurrentOrder_GetOrderCost_Int()
+		{
+			Bread bread = new Bread();
+			bread.MakeOrder(3);
+			Pastry pastry = new Pastry();
+			pastry.MakeOrder(2);
+
+			int result = bread.GetCurrentOrder().cost;
+
+			Assert.AreEqual(result, 19);
+		}
 	}
 }
